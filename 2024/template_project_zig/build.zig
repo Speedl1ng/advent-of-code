f28e22d178d6e62f81bda879626dad9eb8889573
Speedl1ng <justus@ptaszyk.de>
2024-11-30 15:02:40 +0100

    added benchmark setup for 2024

--- 2024/template_project_zig/build.zig
+++ 2024/template_project_zig/build.zig
@@ -3,16 +3,10 @@ const std = @import("std");
 const project_name = "template_project_zig";
 
 pub fn build(b: *std.Build) void {
+    const zbench_module = b.dependency("zbench", .{}).module("zbench");
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    const lib = b.addStaticLibrary(.{
-        .name = project_name,
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = project_name,
@@ -20,6 +14,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("zbench", zbench_module);
 
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
@@ -30,23 +26,15 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    exe_unit_tests.root_module.addImport("zbench", zbench_module);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- 2024/template_project_zig/src/main.zig
+++ 2024/template_project_zig/src/main.zig
@@ -1,37 +1,22 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
+const zbench = @import("zbench");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const stdout = std.io.getStdOut().writer();
+    var bench = zbench.Benchmark.init(allocator, .{});
+    defer bench.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try bench.add("Template Function", templateFunction, .{});
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
+    try stdout.writeAll("\n");
+    try bench.run(stdout);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
+fn templateFunction(allocator: std.mem.Allocator) void {
+    for (0..1000) |_| {
+        const buf = allocator.alloc(u8, 512) catch @panic("Out of memory");
+        defer allocator.free(buf);
+    }
 }
